0d0dd65cda37fc52945c7c92dd167f91d7b50c0a
Yersultan Moldaspan <98903519+Moldaspan@users.noreply.github.com>
2023-03-08 20:50:17 +0600

    Add files via upload

---
+++ lab6/src/app/models.ts
@@ -0,0 +1,20 @@
+export interface Album {
+  id: number;
+  title: string;
+
+}
+
+
+export interface Photo {
+
+  album_id: number,
+  id: number;
+
+  title: string;
+
+  url: string;
+
+  thumbnailUrl: string;
+}
+
+
