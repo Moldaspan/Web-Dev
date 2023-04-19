83e04b91b887bf850b32d505e61939d3d94ac07d
Ерсултан <ersultanmoldaspanov@mail.ru>
2023-04-19 17:42:44 +0600

    lab10

---
+++ lab10/hh_back/app/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = {
+    path('companies/', company_list),
+    path('companies/<int:id>/', company_id),
+    path('companies/<int:id>/vacancies/', company_vacancies),
+    path('vacancies/', VacanciesAPIView.as_view()),
+    path('vacancies/<int:id>/', VacancyDetailAPIView.as_view()),
+    path('vacancies/top_ten/', top_ten_vacancies)
+}
---
+++ lab10/hh_front/src/app/company/company.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {Company} from "../cmodels";
+import {CompanyService} from "../company.service";
+
+@Component({
+  selector: 'app-company',
+  templateUrl: './company.component.html',
+  styleUrls: ['./company.component.css']
+})
+export class CompanyComponent implements OnInit{
+  companies: Company[]=[];
+
+
+  constructor(private companySer: CompanyService) {}
+
+  ngOnInit(): void {
+    this.companySer.getCompanies().subscribe((data) => {
+      this.companies = data;
+    });
+  }
+}
